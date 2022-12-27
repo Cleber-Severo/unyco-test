b33bf2872657ad847a43fcfce22935e113f609c3
Cléber Severo <clebersevero1998@gmail.com>
2022-12-26 23:17:53 -0300

    setting main port to 9004 as requested and fetching info from the API via axios with the headers infos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
 
 function App() {
+
+   const getAPI = async () => {
+    
+    try {
+     
+      const res = await axios.get('https://api.beta.unycos.com/u/courses/spotlights/natacion',{
+      
+        headers: {
+          'Content-Type' : 'application/json',
+          'x-mejor-key' : 'unycos'
+        }
+
+      });
+      
+      console.log(res.data.spotlights);
+
+    } catch (error) {
+     
+    }
+  };
+
+  useEffect(() => {
+    getAPI();
+  }, [])
+  
   return (
+    
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+     
     </div>
   );
 }
