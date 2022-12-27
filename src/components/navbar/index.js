2b2f1d6f366902f478bf93281cd281c070555440
Cléber Severo <clebersevero1998@gmail.com>
2022-12-26 23:38:01 -0300

    initial styling to mobile navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './navbar.css'
 
 const Navbar = () => {
   return (
     <div className='Navbar'>
-        <img src='/images/unycos-logo.svg' alt="" />
-        <img src='/images/bx-menu.svg' alt="" />
+        <img src='/images/unycos-logo.svg' alt="main logo" className='Navbar__logo' />
+        <img src='/images/bx-menu.svg' alt="hamburger menu" className='Navbar_menu'/>
     </div>
   )
 }
