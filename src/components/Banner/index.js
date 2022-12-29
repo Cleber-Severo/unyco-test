bf0bcf6882543bc1c8f28cfa2c9b220ea2e33ccd
Cléber Severo <clebersevero1998@gmail.com>
2022-12-29 19:38:04 -0300

    banner content and html tags content without styling

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -4,8 +4,21 @@ import './banner.css'
 const Banner = () => {
   return (
     <div className='banner'>
-      <div className="banner__image"></div>
-
+      <div className="banner__image">
+        <h1>MIREIA BELMONTE</h1>
+        <h2>enseña natación</h2>
+      </div>
+        <button>SOLICITAR INFORMACIÓN</button>
+        <div className="banner__icons">
+          <div className="icon-modules">
+            <div className="book-icon"></div>
+            <h4>12 MÓDULOS</h4>
+          </div>
+          <div className="icon-hours">
+            <div className="film-icon"></div>
+            <h4>+3,5 horas</h4> 
+          </div>
+        </div>
     </div>
   )
 }
