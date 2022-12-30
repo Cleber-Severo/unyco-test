22293cff621daa4e2cb2b954d8e1b44f1fdbd6bf
Cléber Severo <clebersevero1998@gmail.com>
2022-12-30 14:19:55 -0300

    Form working on mobile and desktop versions

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,6 +4,7 @@ import './Form.css'
 const Form = () => {
   return (
     <form>
+        <h3 className='form__title'>Obtén más información</h3>
         <input type="text" placeholder='Nombre completo' id="" />
         <input type="text" placeholder='Correo electrónico' id="" />
         <select placeholder='País / Código país' id="">
@@ -12,7 +13,18 @@ const Form = () => {
             <option value="ARG">Argentina</option>
         </select>
         <input type="tel" placeholder='TeLéfono' id="" />
-        <button type='submit'>ENVIAR</button>
+        
+        <div className='form__checkboxes'>
+            <input type="checkbox" id='data-confidentiality'/>
+            <label htmlFor="data-confidentiality">Acepto las condiciones de uso y protección de datos.</label>
+        </div>
+
+        <div className='form__checkboxes'>
+            <input type="checkbox" id='info-agreement'/>
+            <label htmlFor="info-agreement">Me gustaría recibir información sobre nuevos cursos, ofertas y promociones</label>
+        </div>
+
+        <button type='submit' onClick={(e) => e.preventDefault()} >ENVIAR</button>
     </form>
   )
 }
