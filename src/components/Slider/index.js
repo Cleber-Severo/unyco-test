8f275b0fd79361f078c37e0ae8ffd009f6a53379
Cléber Severo <clebersevero1998@gmail.com>
2022-12-30 14:44:39 -0300

    slider fix

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -2,11 +2,43 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import './slider.css'
 
-const Slider = ({courses}) => {
+const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
   
-  const {  title, description, image } = courses[currentSlide]
+  const getAPI = async () => {
+    
+    try {
+     
+      const res = await axios.get('https://api.beta.unycos.com/u/courses/spotlights/natacion',{
+      
+        headers: {
+          'Content-Type' : 'application/json',
+          'x-mejor-key' : 'unycos'
+        }
+        
+      });
+      
+      
+      setCourses(res.data.spotlights);
+      setLoading(false)
+    } catch (error) {
+      
+    }
+  };
+  
+  useEffect(() => {
+    getAPI();
+  }, [])
+  
+  
+  if(loading) {
+    return <h2>Loading ...</h2>
+  }
 
+  const {  title, description, image } = courses[currentSlide];
+  
   return (
     <section className='Slider'>
 
